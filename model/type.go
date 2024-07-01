011a5765af959b393c6006ee4254f930ef807301
rayfanaqbil <rayfana09@gmail.com>
2024-07-01 19:08:29 +0700

    add : struct Credentials

--- model/type.go
+++ model/type.go
@@ -24,6 +24,11 @@ type Developer struct {
 	Bio  string             `bson:"dev_bio,omitempty" json:"bio,omitempty"`
 }
 
+type Credentials struct {
+    Username string 				`bson:"user_name,omitempty" json:"username,omitempty"`
+    Password string 				`bson:"password,omitempty" json:"password,omitempty"`
+}
+
 type Admin struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
     User_name string             `bson:"user_name,omitempty" json:"user_name,omitempty"`
